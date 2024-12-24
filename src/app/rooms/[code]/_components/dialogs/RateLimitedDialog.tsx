cdaaeb8f0bc606c4d61f44d8b90bbac3014d4077
Nathan <52774360+NDB-7@users.noreply.github.com>
2024-12-24 11:27:14 -0800

    Added rate limiting

---
+++ src/app/rooms/[code]/_components/dialogs/RateLimitedDialog.tsx
@@ -0,0 +1,31 @@
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+
+export default function RateLimitedDialog({
+  open,
+  close,
+}: {
+  open: boolean;
+  close: () => void;
+}) {
+  return (
+    <Dialog open={open}>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>You've been rate limited!</DialogTitle>
+          <DialogDescription>
+            Please refrain from spamming messages, or you may be removed from
+            the chatroom.
+          </DialogDescription>
+        </DialogHeader>
+        <Button onClick={close}>OK</Button>
+      </DialogContent>
+    </Dialog>
+  );
+}
