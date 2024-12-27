b2ccd8f4e3d03808cf4aba4482bed62cd3964f3b
Nathan <52774360+NDB-7@users.noreply.github.com>
2024-12-27 08:59:05 -0800

    Landing page nav complete

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import "./customClasses.css";
 
 const inter = Inter({
   weight: ["400", "700"],
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,32 @@
+import Image from "next/image";
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <div>
-      <h1>Welcome.</h1>
-    </div>
+    <nav className="border-b-2  border-gray-200 fixed w-full h-16">
+      <ul className="max-w-7xl h-full mx-auto px-6 flex items-center">
+        <li>
+          <Link href="/">
+            <Image
+              src="/images/LogoFull.svg"
+              alt="logo"
+              width={173}
+              height={30}
+            />
+          </Link>
+        </li>
+        <div className="ml-auto flex items-center gap-8">
+          <li className="hover-link">
+            <Link href="#features">Features</Link>
+          </li>
+          <li className="hover-link">
+            <Link href="#faq">FAQ</Link>
+          </li>
+          <li className="hover-link">
+            <Link href="#contact">Contact</Link>
+          </li>
+        </div>
+      </ul>
+    </nav>
   );
 }
